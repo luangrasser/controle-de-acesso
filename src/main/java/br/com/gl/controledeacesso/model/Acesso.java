9c654fce0a62ffaed250fb5f0ced7e61c9b70fd3
luangrasser <bonilhaluan@gmail.com>
2020-06-27 11:31:03 -0300

    Correção dos endpoints de pesquisar e apagar do resource Acesso

--- src/main/java/br/com/gl/controledeacesso/model/Acesso.java
+++ src/main/java/br/com/gl/controledeacesso/model/Acesso.java
@@ -14,6 +14,8 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Acesso implements Serializable {
 
     private static final long serialVersionUID = 1L;
